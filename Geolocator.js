0832c67cce8f9dea1b97b92cd4b05036928adced
jpc20001 <127573156+jpc20001@users.noreply.github.com>
2023-03-10 19:07:14 -0700

    Add files via upload

---
+++ Geolocator.js
@@ -0,0 +1,17 @@
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    const message = `Your current location is: ${latitude}, ${longitude}`;
+  
+    alert(message);
+  }
+  
